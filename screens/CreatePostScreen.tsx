7a573f687e706c6a68209d3ccb4097cb23b4a70b
plvnplvn <ci.patsakorn_st@tni.ac.th>
2024-08-31 11:31:38 +0700

    Lecture8_Practice2

---
+++ screens/CreatePostScreen.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View ,Button ,TextInput} from 'react-native'
+import React from 'react';
+
+const CreatePostScreen = ({navigation,route}:any):React.JSX.Element => {
+    const [postText,setPostText] = React.useState("");
+  return (
+    <>
+     <TextInput
+      multiline
+      placeholder='Tell something...'
+      style={{height:200,padding:10,backgroundColor:'white'}}
+      value={postText}
+      onChangeText={setPostText}
+     />
+     <Button
+      title="Done"
+      onPress={()=>{
+        navigation.navigate({
+            name:'Home',
+            params:{post:postText}});
+        }}
+        />
+        </>
+  );
+};
+
+export default CreatePostScreen;
+
+const styles = StyleSheet.create({})
